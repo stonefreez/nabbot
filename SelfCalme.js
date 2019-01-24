be0006ab4a0c75eeef440c293d1b377710d62567
stonefreez <36804186+stonefreez@users.noreply.github.com>
2019-01-24 18:30:56 +0100

    Update SelfCalme.js

--- SelfCalme.js
+++ SelfCalme.js
@@ -1,4 +1,4 @@
-﻿const Discord = require("discord.js"),
+const Discord = require("discord.js"),
 bot = new Discord.Client(),
 express = require("express"),
 gifSearch = require("gif-search"),
@@ -6,6 +6,8 @@ sayembed = new Discord.RichEmbed(),
 ms = require("ms"),
 con = console.log,
 token = "NDc0MzMwNTcwODk4NTM4NDk2.Dyo1Tw.GWUr-pWKe-OueESgkkfPzvVWQXw",
+kiss = ["https://cdn.weeb.sh/images/SJ--2auDZ.gif", "https://cdn.weeb.sh/images/Skc42pdv-.gif", "https://cdn.weeb.sh/images/rJ_U2p_Pb.gif", "https://cdn.weeb.sh/images/ryFdQRtF-.gif", "https://cdn.weeb.sh/images/SkQIn6Ovb.gif", "https://cdn.weeb.sh/images/SJQRoTdDZ.gif", "https://cdn.weeb.sh/images/Sk1k3TdPW.gif", "https://cdn.weeb.sh/images/r1H42advb.gif", "https://cdn.weeb.sh/images/S1E1npuvb.gif", "https://cdn.weeb.sh/images/ByurnpODW.gif", "https://cdn.weeb.sh/images/S1y-4l5Jf.gif", "https://cdn.weeb.sh/images/Skv72TuPW.gif", "https://cdn.weeb.sh/images/SJINn6OPW.gif", "https://cdn.weeb.sh/images/SJ3dXCKtW.gif", "https://cdn.weeb.sh/images/H1Gx2aOvb.gif", "https://cdn.weeb.sh/images/S1qZksSXG.gif", "https://cdn.weeb.sh/images/r1cB3aOwW.gif", "https://cdn.weeb.sh/images/HJ8dQRYK-.gif", "https://cdn.weeb.sh/images/BJLP3a_Pb.gif"],
+hug = ["https://s-media-cache-ak0.pinimg.com/originals/49/a2/1e/49a21e182fcdfb3e96cc9d9421f8ee3f.gif", "https://s-media-cache-ak0.pinimg.com/originals/16/46/f7/1646f720af76ea58853ef231028bafb1.gif", "https://media.giphy.com/media/xJlOdEYy0r7ZS/giphy.gif", "http://i.imgur.com/2WywS3T.gif", "http://i.imgur.com/8ruodNJ.gif", "https://myanimelist.cdn-dena.com/s/common/uploaded_files/1461071296-7451c05f5aae134e2cceb276b085a871.gif", "http://i0.kym-cdn.com/photos/images/original/000/931/030/394.gif", "https://media.tenor.co/images/1171c186f9130d1efa4a186ad4371e8c/tenor.gif", "http://cdn.smosh.com/sites/default/files/ftpuploads/bloguploads/0413/epic-hugs-friends-pikachu.gif", "https://cdn.weeb.sh/images/rJaog0FtZ.gif", "https://cdn.weeb.sh/images/B10Tfknqf.gif", "https://cdn.weeb.sh/images/S1a0DJhqG.gif", "https://cdn.weeb.sh/images/Hk4qu_XvZ.gif", "https://cdn.weeb.sh/images/Hk0yFumwW.gif", "https://cdn.weeb.sh/images/BJCCd_7Pb.gif", "https://cdn.weeb.sh/images/BJ0UovdUM.gif"],
 rnb = require('random-number'),
 DiscordPermissions = [
   "ADMINISTRATOR", "CREATE_INSTANT_INVITE", "KICK_MEMBERS",
@@ -25,19 +27,19 @@ commandIntervals = [];
 bot.login(token)
   
 bot.on('ready',() => {
-  bot.user.setActivity('Eaucalme <3', { type: 'STREAMING', url: 'https://www.twitch.tv/eaucalmelpb' });
+  bot.user.setActivity('@̠̠̠̠̿̿̿𝑬̠̠̠̠̿̿̿𝒂̠̠̠̠̿̿̿𝒖̠̠̠̠̿̿̿𝒄̠̠̠̠̿̿̿𝒂̠̠̠̠̿̿̿𝒍̠̠̠̠̿̿̿𝒎̠̠̠̠̿̿̿𝒆̠̠̠̠̿̿̿_̠̠̠̠̿̿̿ ̠̠̠̠̿̿̿𝑶̠̠̠̠̿̿̿𝒏̠̠̠̠̿̿̿ ̠̠̠̠̿̿̿𝑻̠̠̠̠̿̿̿𝒘̠̠̠̠̿̿̿𝒊̠̠̠̠̿̿̿𝒕̠̠̠̠̿̿̿𝒕̠̠̠̠̿̿̿𝒆̠̠̠̠̿̿̿𝒓̠̠̠̠̿̿̿ ̠̠̠̠̿̿̿<̠̠̠̠̿̿̿3', { type: 'STREAMING', url: 'https://www.twitch.tv/eaucalmelpb' });
   let memberCount = bot.users.size;
   let serverCount = bot.guilds.size;
   con(
     `${"-".repeat(40)}\n`                         + 
-    "|  Launching SelfCalme   |\n"                + 
+    "|  Bienvenue sur: Kraken Selfbot-Logs.  |\n" + 
     `${"-".repeat(40)}\n`                         +  
-    "Information du compte : \n"                  +
-    `Nom du comptes    : ${bot.user.tag}!\n`      + 
-    `ID du comptes    : ${bot.user.id}\n`         +
-    `Token: du comptes : ${bot.token}\n`          +
+    "Information du bot : \n"                     +
+    `Nom du bot    : ${bot.user.tag}!\n`          + 
+    `ID du bot     : ${bot.user.id}\n`            +
+    `Token: du bot : ${bot.token}\n`              +
     `${"-".repeat(40)}\n`                         +
-    "Stats comptes : \n"                          +
+    "Stats bot : \n"                              +
     `Utilisateurs : ${memberCount}\n`             +
     `Serveurs     : ${serverCount}\n`             +
     `${"-".repeat(40)}\n`
